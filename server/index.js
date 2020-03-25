76bd42bb257514408de783afe07a37bb7370fd5c
jirwincline <jci@pdx.edu>
2020-03-25 16:35:37 -0700

    add basic post

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
+
+//ROUTES//
+
+//create todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+// get all todo
+
+//get a todo
+
+//update a todo
+
+// delete a todo
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
