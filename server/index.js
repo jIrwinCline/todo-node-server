027464fd9686b16d68df3198a863f787fa70e640
jirwincline <jci@pdx.edu>
2020-03-25 19:10:37 -0700

    created connection to db

--- server/index.js
+++ server/index.js
@@ -13,7 +13,12 @@ app.use(express.json());
 
 app.post("/todos", async (req, res) => {
   try {
-    console.log(req.body);
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1)",
+      [description]
+    );
+    res.json(newTodo);
   } catch (err) {
     console.error(err.message);
   }
