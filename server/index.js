f8030c82b7b96f56383dee973b92189af52d539e
jirwincline <jci@pdx.edu>
2020-03-26 18:14:19 -0700

    add get todo by id

--- server/index.js
+++ server/index.js
@@ -35,6 +35,17 @@ app.get("/todos", async (req, res) => {
 });
 
 //get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.log(error);
+  }
+});
 
 //update a todo
 
