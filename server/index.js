2e9e37c64e669315788a7c8925d906b4d6161890
jirwincline <jci@pdx.edu>
2020-03-26 20:17:29 -0700

    add delete route

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ app.put("/todos/:id", async (req, res) => {
     const { id } = req.params;
     const { description } = req.body;
     const updateTodo = await pool.query(
-      "UPDATE todo SET description = $1 WHERE todo_id =$2",
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
       [description, id]
     );
     res.json("Todo was updated");
@@ -62,6 +62,19 @@ app.put("/todos/:id", async (req, res) => {
   }
 });
 // delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id
+    ]);
+
+    res.json("Todo was deleted");
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
